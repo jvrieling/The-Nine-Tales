d12e3807bf606006cee0bb0404cf648d6cb35cb1
ErrorMaster <axallami@gmail.com>
2022-02-04 10:33:11 -0500

    Added Crusher

---
+++ The Nine Tales/Assets/_TheNineTales/Personal/Abdullah/Scripts/Crusher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher : MonoBehaviour
+{
+    float moveSpeed = 2;
+    float moveDirection = 1;
+    float Range = 4;
+    Vector3 OriginalPoisition;
+    Vector3 TargetPoisition;
+
+    void Start()
+    {
+        OriginalPoisition = transform.position;
+        TargetPoisition = OriginalPoisition + new Vector3(0, Range, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(transform.position.y < OriginalPoisition.y)
+        {
+            moveDirection = 1;
+        }
+        if (transform.position.y > TargetPoisition.y)
+        {
+            moveDirection = -1;
+        }
+
+        transform.position = transform.position + new Vector3(0, moveDirection * moveSpeed, 0);
+    }
+}
