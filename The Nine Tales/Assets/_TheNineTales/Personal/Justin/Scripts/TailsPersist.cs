ae88ee747e7b6bbe750be10a0ca188eb4034d165
jvrieling <32886119+jvrieling@users.noreply.github.com>
2022-02-15 17:15:26 -0500

    Make fox tails persist between scenes

---
+++ The Nine Tales/Assets/_TheNineTales/Personal/Justin/Scripts/TailsPersist.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailsPersist : MonoBehaviour
+{
+    private static bool[] tailsEnabled;
+
+    private void Awake()
+    {
+        if (tailsEnabled == null)
+        {
+            tailsEnabled = new bool[9];
+            tailsEnabled[0] = true;
+        }
+    }
+
+    public void OnDisable()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            tailsEnabled[i] = transform.GetChild(i).gameObject.activeSelf;
+        }
+    }
+
+    public void OnEnable()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(tailsEnabled[i]);
+        }
+    }
+}
