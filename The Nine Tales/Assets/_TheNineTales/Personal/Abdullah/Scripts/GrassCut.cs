e3069c9c4cd882a6bca4327b3f6293c5713b504b
jvrieling <32886119+jvrieling@users.noreply.github.com>
2022-03-25 11:49:10 -0400

    Add the grass getting cut effect

--- The Nine Tales/Assets/_TheNineTales/Personal/Abdullah/Scripts/GrassCut.cs
+++ The Nine Tales/Assets/_TheNineTales/Personal/Abdullah/Scripts/GrassCut.cs
@@ -13,49 +13,59 @@ public class GrassCut : MonoBehaviour
     private static List<int> destroyedPersistants;
 
     public AK.Wwise.Event cutSound;
+    public GameObject cutVFX;
+
+    private Animator an;
     private void Awake()
     {
         if (destroyedPersistants == null)
             destroyedPersistants = new List<int>();
+
+        an = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
-        if(persistant && destroyedPersistants.Contains(id))
+        if (persistant && destroyedPersistants.Contains(id))
         {
             Destroy(gameObject);
         }
     }
-
-    void OnTriggerEnter2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.GetComponent<Player>() != null)
-        {
-            if (col.GetComponent<Player>().isDashing == true)
-            {
-                Player.inventory.AddItem(GrassCuttable);
-                 if (persistant) destroyedPersistants.Add(id);
-
-                cutSound.Post(gameObject);
-
-                Destroy(gameObject);
-
-            }
-
-        }
+        CheckCut(col);
     }
     private void OnTriggerStay2D(Collider2D col)
     {
+        CheckCut(col);
+    }
+
+    public void CheckCut(Collider2D col)
+    {
         if (col.GetComponent<Player>() != null)
         {
             if (col.GetComponent<Player>().isDashing == true)
             {
+                //Add grass to the inventory
                 Player.inventory.AddItem(GrassCuttable);
                 if (persistant) destroyedPersistants.Add(id);
-                cutSound.Post(gameObject);
-                Destroy(gameObject);
-            }
 
+                //Play cutting effects
+                cutSound.Post(gameObject);
+                Instantiate(cutVFX, transform.position + new Vector3(0, Random.Range(0.4f, 1f), 0), Quaternion.identity);
+
+                //If there's an animator, trigger the destroy animation.
+                if (an != null) an.SetTrigger("Dest");
+                else Destroy(gameObject);
+            }
         }
+    }
+
+    /// <summary>
+    /// Used by the animator to destroy the game object.
+    /// </summary>
+    public void RemoveGrass()
+    {
+        Destroy(gameObject);
     }
 }
