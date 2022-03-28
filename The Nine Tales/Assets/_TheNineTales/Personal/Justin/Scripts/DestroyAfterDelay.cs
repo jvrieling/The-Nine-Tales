6d043bb8b62098f4cbcac6280c89eab30416ff0f
jvrieling <32886119+jvrieling@users.noreply.github.com>
2022-03-28 16:32:14 -0400

    Implement spiders & web art

---
+++ The Nine Tales/Assets/_TheNineTales/Personal/Justin/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    public float delay = 3f;
+    void OnEnable()
+    {
+        Destroy(gameObject, delay);
+    }
+}
